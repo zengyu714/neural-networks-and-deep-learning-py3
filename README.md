6c03327269996a9a2fd693bdc9232b7ff4e2c750
zengyu714 <zykimmy714@gmail.com>
2017-02-27 16:05:24 +0800

    Add four important eqs
    Load MNIST data in numpy.ndarray type, and take notes of the four
    important backpropagation equations.

--- README.md
+++ README.md
@@ -10,3 +10,36 @@ Why it?
 How?
 ---
 Keep it simple and STUPID
+<br><br>
+
+ :full_moon_with_face:
+ :new_moon_with_face: :new_moon_with_face: :new_moon_with_face: :new_moon_with_face: :new_moon_with_face: :new_moon_with_face: :new_moon_with_face: :new_moon_with_face: :new_moon_with_face: :new_moon_with_face: :new_moon_with_face: :new_moon_with_face: :new_moon_with_face: :new_moon_with_face: :new_moon_with_face:
+
+
+ Backpropagation Equations
+---
+:fire: error $\delta_j^l$ of neuron $j$ in layer $l$
+$$
+\delta_j^l\equiv \dfrac{\partial C}{\partial z^l_j}
+$$
+
+:heavy_check_mark: error $\delta_j^L$ of neuron $j$ in the output layer $L $ <br>
+$$
+\delta_j^l = \dfrac{\partial C}{\partial z^L_j} \ \sigma'(z^L_j)\tag{BP1}
+$$
+
+:heavy_check_mark: error $\delta^l$ in terms of the error in the next layer,  $\ \delta^{l+1}\\ $
+$$
+\delta^l = (w^{l+1})^T\delta^{l+1})\ \odot\ \sigma'(z^l)\tag{BP2}
+$$
+
+:white_check_mark: **An equation for the rate of change of the cost with respect to any bias in the network**<br>
+$$
+\dfrac{\partial C}{\partial b^l_j}= \delta_j^l \tag{BP3}
+$$
+
+
+:white_check_mark: **An equation for the rate of change of the cost with respect to any weight in the network**<br>
+$$
+\dfrac{\partial C}{w_{jk}^l}= a^{l-1}_k \delta^l_j \tag{BP4}
+$$
--- mnist_loader.py
+++ mnist_loader.py
@@ -4,18 +4,26 @@ import gzip
 
 import numpy as np
 
+def one_hot(true_label):
+    one_hot_list = [int(i == true_label) for i in range(10)]
+    return np.reshape(one_hot_list, (10, 1))
+
 def load_data():
     """
     training_data, validation_data, test_data = load_data()
 
     This function generates the MNIST data sequentially
     e.g.
+    ------------------------------------------------------------------------
     training_data:   (raw_pixels, labels)
-                      both are **np.ndarray** type with size 50000 x 784 and 50000 x 10
-    validation_data: (raw_pixels, labels)
-    /test_data        both are **np.ndarray** type with size 10000 x 784 and 10000 x 1
-
+    type: numpy.ndarray with size (784, 1), numpy.ndarray with size (10, 1)
+    ------------------------------------------------------------------------
+    validation_data: (raw_pixels, label )
+    test_data      : (raw_pixels, label )
+    type: numpy.ndarray with size (784, 1), numpy.int64
+    ------------------------------------------------------------------------
     """
+
     with gzip.open("mnist.pkl.gz", "rb") as f:
         total_data = pickle.load(f, encoding="latin1")
         # total_data contains training_data, validation_data, test_data
@@ -24,18 +32,12 @@ def load_data():
         # training_data[1].shape == (50000, 1)
 
     for data in total_data:
-        data_x = data[0]
+        data_x = [x.reshape(784, 1) for x in data[0]]
         data_y = data[1]
 
         # if is training_data, make its label one-hot
-        # --------------------------------------------------------------
-        # def one_hot(true_label):
-        #     return np.array([int(i == true_label) for i in range(10)])
-        # --------------------------------------------------------------
+        if len(data_x) == 50000:
+            data_y = [one_hot(y) for y in data[1]]
 
-        if len(data[0]) == 50000:
-            data_y = [[int(i == y) for i in range(10)]
-                          for y in data[1]]
-
-        data = (data_x, data_y)
+        data = list(zip(data_x, data_y))
         yield data
